b682076701f0121b5f182bd0faa77db06a556d6b
anagheshm <anagheshm@gmail.com>
2020-10-13 12:07:20 +0530

    CI test

--- Source/random-number-generator/src/main/java/com/main/MainClass.java
+++ Source/random-number-generator/src/main/java/com/main/MainClass.java
@@ -8,7 +8,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainClass.class, args);
-		System.out.println("----Consolase Logs----");
+		System.out.println("----Consolsdase Logs----");
 		System.out.println("Server started on port: 8080");
 		System.out.println("Status: 200 OK");
 
