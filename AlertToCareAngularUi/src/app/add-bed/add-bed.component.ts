80dba8b176030ec822e546f2e99f6435c08bcc54
kiran-kumar-r <61319940+kiran-kumar-r@users.noreply.github.com>
2020-10-29 21:15:55 +0530

    Add files via upload

--- AlertToCareAngularUi/src/app/add-bed/add-bed.component.ts
+++ AlertToCareAngularUi/src/app/add-bed/add-bed.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Bed } from '../bed';
 import { BedSub } from '../bed-sub';
 import { BedService } from '../bed.service';
 
@@ -11,6 +12,7 @@ import { BedService } from '../bed.service';
 export class AddBedComponent implements OnInit {
 
   bedsub : BedSub = new BedSub() ;
+  bed : Bed ;
 
   constructor( private bedService : BedService,
     private router : Router ) {
@@ -22,9 +24,17 @@ export class AddBedComponent implements OnInit {
   saveBed() {
     this.bedService.addBed( this.bedsub ).subscribe( data=> {
       console.log( data ) ;
-      this.goToBedList() ;   
+      this.goToBedList() ; 
     },
-    error => console.log( error )) ;
+
+    error => {
+      console.log( error ) ;
+      if( isNaN(this.bedsub.icuId) ) {
+        alert( "Please provide valid ICU ID, which should be a number)" ) ;
+      } else {
+        alert( error.error.message + ". Please choose another ICU to allocate this bed." ) ;
+      }
+    }) ;
   }
 
   goToBedList() {
@@ -32,7 +42,7 @@ export class AddBedComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log( this.bedsub ) ;
+    console.log( this.bedsub.icuId ) ;
     this.saveBed() ;
   }
 
